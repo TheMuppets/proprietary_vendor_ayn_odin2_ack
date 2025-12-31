d3841203e8fc4018d91b8661966cd72561368a7b
Aaron Kling <webgeek1234@gmail.com>
2025-12-31 13:10:15 -0600

    Add odin2portal audio firmware

--- odin2_ack-vendor.mk
+++ odin2_ack-vendor.mk
@@ -12,6 +12,11 @@ PRODUCT_COPY_FILES += \
     vendor/ayn/odin2_ack/proprietary/vendor/firmware/qcom/sm8550/ayntec/odin2/adspua.jsn:$(TARGET_COPY_OUT_VENDOR)/firmware/qcom/sm8550/ayntec/odin2/adspua.jsn \
     vendor/ayn/odin2_ack/proprietary/vendor/firmware/qcom/sm8550/ayntec/odin2/aw883xx_acf.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/qcom/sm8550/ayntec/odin2/aw883xx_acf.bin \
     vendor/ayn/odin2_ack/proprietary/vendor/firmware/qcom/sm8550/ayntec/odin2/battmgr.jsn:$(TARGET_COPY_OUT_VENDOR)/firmware/qcom/sm8550/ayntec/odin2/battmgr.jsn \
+    vendor/ayn/odin2_ack/proprietary/vendor/firmware/qcom/sm8550/ayntec/odin2portal/adspr.jsn:$(TARGET_COPY_OUT_VENDOR)/firmware/qcom/sm8550/ayntec/odin2portal/adspr.jsn \
+    vendor/ayn/odin2_ack/proprietary/vendor/firmware/qcom/sm8550/ayntec/odin2portal/adsps.jsn:$(TARGET_COPY_OUT_VENDOR)/firmware/qcom/sm8550/ayntec/odin2portal/adsps.jsn \
+    vendor/ayn/odin2_ack/proprietary/vendor/firmware/qcom/sm8550/ayntec/odin2portal/adspua.jsn:$(TARGET_COPY_OUT_VENDOR)/firmware/qcom/sm8550/ayntec/odin2portal/adspua.jsn \
+    vendor/ayn/odin2_ack/proprietary/vendor/firmware/qcom/sm8550/ayntec/odin2portal/aw883xx_acf.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/qcom/sm8550/ayntec/odin2portal/aw883xx_acf.bin \
+    vendor/ayn/odin2_ack/proprietary/vendor/firmware/qcom/sm8550/ayntec/odin2portal/battmgr.jsn:$(TARGET_COPY_OUT_VENDOR)/firmware/qcom/sm8550/ayntec/odin2portal/battmgr.jsn \
     vendor/ayn/odin2_ack/proprietary/vendor/firmware/qcom/sm8550/ayntec/thor/adspr.jsn:$(TARGET_COPY_OUT_VENDOR)/firmware/qcom/sm8550/ayntec/thor/adspr.jsn \
     vendor/ayn/odin2_ack/proprietary/vendor/firmware/qcom/sm8550/ayntec/thor/adsps.jsn:$(TARGET_COPY_OUT_VENDOR)/firmware/qcom/sm8550/ayntec/thor/adsps.jsn \
     vendor/ayn/odin2_ack/proprietary/vendor/firmware/qcom/sm8550/ayntec/thor/adspua.jsn:$(TARGET_COPY_OUT_VENDOR)/firmware/qcom/sm8550/ayntec/thor/adspua.jsn \
