793292540df3ecb6d724c99f37d4a2e5e3b82765
Aaron Kling <webgeek1234@gmail.com>
2026-03-05 22:21:28 -0600

    Add retroid pocket 6 audio firmware

--- odin2_ack-vendor.mk
+++ odin2_ack-vendor.mk
@@ -26,4 +26,9 @@ PRODUCT_COPY_FILES += \
     vendor/ayn/odin2_ack/proprietary/vendor/firmware/qcom/sm8550/ayntec/thor/adsps.jsn:$(TARGET_COPY_OUT_VENDOR)/firmware/qcom/sm8550/ayntec/thor/adsps.jsn \
     vendor/ayn/odin2_ack/proprietary/vendor/firmware/qcom/sm8550/ayntec/thor/adspua.jsn:$(TARGET_COPY_OUT_VENDOR)/firmware/qcom/sm8550/ayntec/thor/adspua.jsn \
     vendor/ayn/odin2_ack/proprietary/vendor/firmware/qcom/sm8550/ayntec/thor/aw883xx_acf.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/qcom/sm8550/ayntec/thor/aw883xx_acf.bin \
-    vendor/ayn/odin2_ack/proprietary/vendor/firmware/qcom/sm8550/ayntec/thor/battmgr.jsn:$(TARGET_COPY_OUT_VENDOR)/firmware/qcom/sm8550/ayntec/thor/battmgr.jsn
+    vendor/ayn/odin2_ack/proprietary/vendor/firmware/qcom/sm8550/ayntec/thor/battmgr.jsn:$(TARGET_COPY_OUT_VENDOR)/firmware/qcom/sm8550/ayntec/thor/battmgr.jsn \
+    vendor/ayn/odin2_ack/proprietary/vendor/firmware/qcom/sm8550/retroidpocket/rp6/adspr.jsn:$(TARGET_COPY_OUT_VENDOR)/firmware/qcom/sm8550/retroidpocket/rp6/adspr.jsn \
+    vendor/ayn/odin2_ack/proprietary/vendor/firmware/qcom/sm8550/retroidpocket/rp6/adsps.jsn:$(TARGET_COPY_OUT_VENDOR)/firmware/qcom/sm8550/retroidpocket/rp6/adsps.jsn \
+    vendor/ayn/odin2_ack/proprietary/vendor/firmware/qcom/sm8550/retroidpocket/rp6/adspua.jsn:$(TARGET_COPY_OUT_VENDOR)/firmware/qcom/sm8550/retroidpocket/rp6/adspua.jsn \
+    vendor/ayn/odin2_ack/proprietary/vendor/firmware/qcom/sm8550/retroidpocket/rp6/aw883xx_acf.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/qcom/sm8550/retroidpocket/rp6/aw883xx_acf.bin \
+    vendor/ayn/odin2_ack/proprietary/vendor/firmware/qcom/sm8550/retroidpocket/rp6/battmgr.jsn:$(TARGET_COPY_OUT_VENDOR)/firmware/qcom/sm8550/retroidpocket/rp6/battmgr.jsn
